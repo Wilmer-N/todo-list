6cf3effbe029a7ca1e737b8e3c510d1b5b3d39df
Wilmer <wilmernorling@gmail.com>
2022-11-05 00:28:01 +0100

    addig projects

--- src/index.js
+++ src/index.js
@@ -1,11 +1,56 @@
 import './style.css';
-import { createSideBar, createHeader, createTodos } from './layout';
+import { createSideBar, createHeader, createTodos, createProjectDiv, displayWhatToAdd} from './layout';
 
 const content = document.querySelector("#content")
 
 createHeader()
 createSideBar()
 createTodos()
+displayWhatToAdd()
+
+const addBtn = document.querySelector("#add-project")
+const sidebar = document.querySelector("#sidebar")
+const projectInput = document.querySelector("#title-input")
+const bigCard = document.querySelector("#big-card")
+const submitProjectBtn = document.querySelector("#project-submit")
 
 
-console.log("hello")
+const projects = []
+
+function CreateProject(title){
+    projects.push(title)
+    const todoArray = new Array()
+    todoArray.push(title)
+
+    return{
+        title,
+        todoArray
+    }
+}
+console.log(bigCard)
+
+addBtn.addEventListener("click", function(){
+    bigCard.style.display = "initial"
+})
+
+submitProjectBtn.addEventListener("click", function(){
+    const project = CreateProject(projectInput.value)
+    projectInput.value = ""
+    displayProjects()
+    bigCard.style.display = "none"
+})
+
+
+function displayProjects(){
+    //Removes all current divs//
+    const projectDivs = document.querySelectorAll(".sidebar-project")
+    projectDivs.forEach(sideBarprojectDiv => {
+        sideBarprojectDiv.remove()
+    });
+    //-----------//
+    //creates new div
+    projects.forEach(project => {
+        createProjectDiv(project)
+    });
+    console.log(projects)
+}
--- src/layout.js
+++ src/layout.js
@@ -2,6 +2,10 @@ function createSideBar(){
     const sidebar = document.createElement("div")
     sidebar.setAttribute("id", "sidebar")
     content.appendChild(sidebar)
+    const addBtn = document.createElement("button")
+    addBtn.textContent = "Add a project"
+    addBtn.setAttribute("id", "add-project")
+    sidebar.appendChild(addBtn)
 }
 function createHeader(){
     const header = document.createElement("div")
@@ -14,4 +18,24 @@ function createTodos(){
     content.appendChild(todos)
 }
 
-export {createSideBar, createHeader, createTodos}
+function createProjectDiv(title){
+    const project = document.createElement("div")
+    project.setAttribute("class", "sidebar-project")
+    project.textContent = `${title}`
+    sidebar.appendChild(project)
+}
+
+function displayWhatToAdd(){
+    const card = document.createElement("div")
+    card.setAttribute("id", "big-card")
+    content.appendChild(card)
+    const titleInput = document.createElement("input")
+    titleInput.setAttribute("id", "title-input")
+    card.appendChild(titleInput)
+    const submitProject = document.createElement("button")
+    submitProject.setAttribute("id", "project-submit")
+    card.appendChild(submitProject)
+    submitProject.textContent = "submit project"
+}
+
+export {createSideBar, createHeader, createTodos, createProjectDiv, displayWhatToAdd}
